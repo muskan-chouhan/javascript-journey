e9336adcb3976c78672c9673cce57dd07d115153
muskan <mushkanc430@gmail.com>
2025-09-09 00:30:33 +0530

    add number and maths file

---
+++ JavaScript/maths_practice.js
@@ -0,0 +1,12 @@
+console.log(Math.round(7.4));   // ?
+console.log(Math.floor(7.9));   // ?
+console.log(Math.ceil(7.1));    // ?
+console.log(Math.abs(-12));     // ?
+console.log(Math.max(3, 7, 2)); // ?
+console.log(Math.min(3, 7, 2)); // ?
+
+
+//q
+
+let randomNum = Math.floor(Math.random() * 10);
+console.log(randomNum);
