2bbd8c7f83edd9414db6dfa583b674371b3b8b88
muskan <mushkanc430@gmail.com>
2025-09-17 22:19:19 +0530

    add object 1  file

---
+++ JavaScript/object1_practice.js
@@ -0,0 +1,34 @@
+const students = [
+    {
+        id: 1,
+        name: "muskan",
+        age: 18,
+        marks:{
+            maths:80,
+            science:70,
+            english:60
+        },
+    },
+        {
+        id: 2,
+        name: "anyaa",
+        age: 19,
+        marks:{
+            maths:90,
+            science:50,
+            english:80
+        },
+    },
+        {
+        id: 1,
+        name: "nikita",
+        age: 18,
+        marks:{
+            maths:40,
+            science:50,
+            english:40
+        },
+    },
+]
+
+students[1].name;
